9a66164a5e4070a285de1932a389d89e92112052
Engy Ahmed <68815210+engyahmed7@users.noreply.github.com>
2022-01-03 13:29:47 +0200

    Update Contact.js

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -2,56 +2,43 @@ import React from "react";
 import "../styles/Contact.css";
 import bkImg from "../assets/pizzaLeft.jpg";
 import { useRef } from "react";
-
 function Contact() {
   let inputName = useRef();
   let inputEmail = useRef();
   let inputMsg = useRef();
-
-  function submitHandller(event) {
+  function submitHandler(event) {
     event.preventDefault();
-    let nameEnterd = inputName.current.value;
-    let EmailEnterd = inputEmail.current.value;
-    let MsgEnterd = inputMsg.current.value;
+    let nameEntered = inputName.current.value;
+    let mailEntered = inputEmail.current.value;
+    let msgEntered = inputMsg.current.value;
 
-    console.log(nameEnterd, EmailEnterd, MsgEnterd);
+    console.log(nameEntered, mailEntered, msgEntered);
   }
-
   return (
-    <div className="contact">
-      <div
-        className="leftSide"
-        style={{ backgroundImage: `url(${bkImg})` }}
-      ></div>
-      <div className="rightSide">
-        <h1> Contact</h1>
-        <form onSubmit={submitHandller}>
-          <label>Name:</label>
-          <input
-            type="text"
-            required
-            placeholder="full Name"
-            ref={inputName}
-          ></input>
+    <>
+      <div className="contact">
+        <div
+          className="leftSide"
+          style={{ backgroundImage: `url(${bkImg})` }}
+        ></div>
 
-          <label>Email:</label>
-          <input
-            type="email"
-            required
-            placeholder="Mail@Example.com"
-            ref={inputEmail}
-          ></input>
+        <div className="rightSide">
+          <h1>Contact Us</h1>
+          <form onSubmit={submitHandler}>
+            <input type="text" required placeholder=" Name" ref={inputName} />
 
-          <textarea
-            row="2"
-            required
-            placeholder="Your Msg ....."
-            ref={inputMsg}
-          ></textarea>
-          <button type="submit"> Send Message</button>
-        </form>
+            <input type="email" required placeholder="Email" ref={inputEmail} />
+            <textarea
+              row="3"
+              required
+              placeholder="Message..."
+              ref={inputMsg}
+            />
+            <button type="submit">Send Message</button>
+          </form>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 export default Contact;
